b439bad9f398c580373face8d1b3781ee64762e3
Serch16 <Sergio@192.168.1.42>
2014-05-12 11:58:15 +0200

    Cambiada conexión a bbdd

--- src/main/java/net/daw/helper/Conexion.java
+++ src/main/java/net/daw/helper/Conexion.java
@@ -1,14 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package net.daw.helper;
 
-/**
- *
- * @author rafa
- */
 public class Conexion {
 
     public static enum Tipo_conexion {
@@ -22,7 +13,7 @@ public class Conexion {
     }
 
     public static String getDatabaseName() {
-        return "ausiasyield";
+        return "bolsa";
     }
 
     public static String getDatabaseLogin() {
